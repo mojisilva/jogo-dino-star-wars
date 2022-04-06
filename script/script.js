f50f554308b76a39a11b8bb15fca019147291fb0
mojisilva <mojiasilva@gmail.com>
2022-04-06 15:05:51 -0300

    updating project

---
+++ script/script.js
@@ -0,0 +1,75 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+
+function  handleKeyUp(event){
+    if(event.keyCode === 32){
+        if(!isJumping){
+            jump();
+        }
+    }
+}
+
+function jump(){
+    
+
+    let upInterval = setInterval(() =>{
+        if(position >= 150){
+            clearInterval(upInterval); 
+            let downInterval = setInterval(() =>{
+                if(position <=0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }else{
+                    position -=20;
+                    dino.style.bottom = position + 'px';
+                }                
+            }, 20);
+        }else{
+            position += 20; 
+            dino.style.bottom = position + 'px';
+        }
+    },20);
+
+}
+
+
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+    let leftInterval = setInterval(() =>{
+
+        
+        if(cactusPosition <-60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if(cactusPosition >0 && cactusPosition <60 && position <60){
+             
+            clearInterval(leftInterval);
+           gameOver();
+
+
+        }else {
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    },20);
+    setTimeout(createCactus, randomTime);
+}
+
+function gameOver(){
+    const gameOver = document.body.innerHTML = '<h1 class="game-over"><img src="img/game-over.png" alt="game-over"></h1>';
+}
+
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
+
